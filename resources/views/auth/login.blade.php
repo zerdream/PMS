0c402d1e72f1de8aa24145647141eeb1466f56de
zihai <zihai2001@163.com>
2015-07-06 16:39:12 +0800

    createview

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,61 +1,87 @@
-@extends('app')
+<!DOCTYPE html>
+<html lang="zh">
+	<head>
+		<title>嘉恒同心供热收费系统-登录</title>
+		
+		<!-- BEGIN META -->
+		<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="keywords" content="your,keywords">
+		<meta name="description" content="Short explanation about this website">
+		<!-- END META -->
 
-@section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
-				<div class="panel-heading">Login</div>
-				<div class="panel-body">
-					@if (count($errors) > 0)
-						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
+		<!-- BEGIN STYLESHEETS -->
+        <link href='http://fonts.googleapis.com/css?family=Roboto:300italic,400italic,300,400,500,700,900' rel='stylesheet' type='text/css'/>
 
-					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <link type="text/css" rel="stylesheet" href="../../resources/assets/css/modules/materialadmin/css/theme-default/bootstrap-1422823238.css" />
 
-						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
-							<div class="col-md-6">
-								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
+        <link type="text/css" rel="stylesheet" href="../../resources/assets/css/modules/materialadmin/css/theme-default/materialadmin-1422823243.css" />
+
+        <link type="text/css" rel="stylesheet" href="../../resources/assets/css/modules/materialadmin/css/theme-default/font-awesome.min-1422823239.css" />
+
+        <link type="text/css" rel="stylesheet" href="../../resources/assets/css/modules/materialadmin/css/theme-default/material-design-iconic-font.min-1422823240.css" />
+		<!-- END STYLESHEETS -->
+
+
+		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+        <script type="text/javascript" src="http://www.codecovers.eu/assets/js/modules/materialadmin/libs/utils/html5shiv.js?1422823601"></script>
+        <script type="text/javascript" src="http://www.codecovers.eu/assets/js/modules/materialadmin/libs/utils/respond.min.js?1422823601"></script>
+        <![endif]-->
+	</head>
+	<body class="menubar-hoverable header-fixed ">
+	<!-- BEGIN LOGIN SECTION -->
+	<section class="section-account">
+		<div class="img-backdrop" style="background-image: url('../../resources/assets/img/modules/materialadmin/img16.jpg')"></div>
+		<div class="spacer"></div>
+		<div class="card contain-sm style-transparent">
+			<div class="card-body">
+				<div class="row">
+					<div class="col-sm-12">
+						<br/>
+						<span class="text-lg text-bold text-primary"></span>
+						<br/><br/>
+						<form class="form floating-label" action="../" accept-charset="utf-8" method="post">
+							<div class="form-group">
+								<input type="text" class="form-control" id="username" name="username">
+								<label for="username">用户名</label>
 							</div>
-						</div>
-
-						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
-							<div class="col-md-6">
-								<input type="password" class="form-control" name="password">
+							<div class="form-group">
+								<input type="password" class="form-control" id="password" name="password">
+								<label for="password">密码</label>
+								<p class="help-block"><a href="login.html#">忘记密码?</a></p>
 							</div>
-						</div>
+							<br/>
+							<div class="row">
+								<div class="col-xs-6 text-left">
+									<div class="checkbox checkbox-inline checkbox-styled">
+										<label>
+											<input type="checkbox"> <span>记住我</span>
+										</label>
+									</div>
+								</div><!--end .col -->
+								<div class="col-xs-6 text-right">
+									<button class="btn btn-primary btn-raised" type="submit">登 录</button>
+								</div><!--end .col -->
+							</div><!--end .row -->
+						</form>
+					</div><!--end .col -->
+				</div><!--end .row -->
+			</div><!--end .card-body -->
+		</div><!--end .card -->
+	</section>
+	<!-- END LOGIN SECTION -->
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<div class="checkbox">
-									<label>
-										<input type="checkbox" name="remember"> Remember Me
-									</label>
-								</div>
-							</div>
-						</div>
+	<!-- BEGIN JAVASCRIPT -->
+	<script src="../../resources/assets/js/modules/materialadmin/libs/jquery/jquery-1.11.2.min.js"></script>
+	<script src="../../resources/assets/js/modules/materialadmin/libs/jquery/jquery-migrate-1.2.1.min.js"></script>
+	<script src="../../resources/assets/js/modules/materialadmin/libs/bootstrap/bootstrap.min.js"></script>
+	<script src="../../resources/assets/js/modules/materialadmin/libs/spin.js/spin.min.js"></script>
+	<script src="../../resources/assets/js/modules/materialadmin/libs/autosize/jquery.autosize.min.js"></script>
+	<script src="../../resources/assets/js/modules/materialadmin/libs/nanoscroller/jquery.nanoscroller.min.js"></script>
+	<script src="../../resources/assets/js/modules/materialadmin/core/cache/63d0445130d69b2868a8d28c93309746.js"></script>
+	<script src="../../resources/assets/js/modules/materialadmin/core/demo/Demo.js"></script>
+	<!-- END JAVASCRIPT -->
 
-						<div class="form-group">
-							<div class="col-md-6 col-md-offset-4">
-								<button type="submit" class="btn btn-primary">Login</button>
-
-								<a class="btn btn-link" href="{{ url('/password/email') }}">Forgot Your Password?</a>
-							</div>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-@endsection
+	</body>
+</html>
